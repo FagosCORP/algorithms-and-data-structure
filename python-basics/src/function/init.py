785ea580762e22c185844fa4a61da8d8c137e305
ryan <hurleypvp1@gmail.com>
2024-01-29 18:58:45 -0300

    functions

---
+++ python-basics/src/function/init.py
@@ -0,0 +1,31 @@
+# funcoes
+#
+# Sem parametros, sem retorno mas com parametros e função que possui parametros e retorno.
+def funcoes():
+    print("escrevendo funcoes aninonimas")
+
+
+funcoes()
+
+
+def funcParam(nome: str):
+    print(nome)
+
+
+funcParam("ryan")
+
+
+def funcReturn(nome: str):
+    return nome
+
+
+returnVar = funcReturn("ryan")
+
+
+def calcula_energia_gravitaciona(e, g, h, m):
+    return g * m * h * e
+
+
+calcula_energia_gravitaciona(90, 30, 12, 12)
+
+help(calcula_energia_gravitaciona)
