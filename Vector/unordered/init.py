5d03232d137305de476c67140bff549feb5c5d3d
ryan <hurleypvp1@gmail.com>
2024-02-20 10:18:52 -0300

    vector-unordered

--- Vector/unordered/init.py
+++ Vector/unordered/init.py
@@ -6,6 +6,9 @@
 # Inserir um jogador na estrutura quando ele estiver em campo.
 # Verificar se um jogador esta presente, pesquisando o numero do jogador na estrutura.
 # Remover quando um jogador for para o descanso ou casa.
+# So vai remover o valor duplicado da ultima_posicao quando ocorrer uma inserção
+# pois a ultima_posicao foi decrementada
+#
 
 import numpy as np
 
@@ -24,6 +27,32 @@ class Vetor:
             for i in range(self.ultima_posicao + 1):
                 print(self.valores[i])
 
+    def insere(self, valor):
+        if self.ultima_posicao == self.capacidade - 1:
+            print("capacidade maxima atingida")
+        else:
+            self.ultima_posicao += 1
+            self.valores[self.ultima_posicao] = valor
+
+    def pesquisar(self, valor):
+        # busca linear posição por posição
+        for index in range(self.valores + 1):
+            if self.valores[index] == valor:
+                return index
+        return -1
+
+    def excluir(self, valor):
+        index = self.pesquisar(valor)
+        if index == -1:
+            return -1
+        for i in range(index, self.ultima_posicao):
+            self.valores[i] = self.valores[i + 1]
+            self.ultima_posicao -= 1
+
 
 vetor = Vetor(10)
 vetor.imprime()
+
+# imprimir as posicões:
+# E saber o numero de posições que estão cheias
+#
