90988b0d0f579340041e80da64eac1368dbbe947
ryan <hurleypvp1@gmail.com>
2024-02-20 13:30:10 -0300

    vector-unordered

---
+++ Vector/ordened/exercices.py
@@ -0,0 +1,75 @@
+import numpy as np
+
+
+class VetorOrdenado:
+    def __init__(self, capacidade: int) -> None:
+        self.capacidade = capacidade
+        self.ultima_posicao = -1
+        self.valores = np.empty(self.capacidade, dtype=int)
+
+    def imprime(self):
+        if self.ultima_posicao == -1:
+            print("O vetor esta vazio")
+            exit()
+        for i in range(self.ultima_posicao + 1):
+            print(self.valores[i])
+
+    def inserir(self, valor):
+        if self.ultima_posicao == self.capacidade - 1:
+            print("O vetor ja esta cheio")
+            exit()
+        self.ultima_posicao += 1
+
+        # aqui eu to pegando a posicao que vai ser inserido o novo elemento
+        # m  aqui e para ordenar
+        posicao = 0
+        for i in range(self.ultima_posicao + 1):
+            posicao = i
+            if self.valores[i] > valor:
+                break
+            # caso ele nao achar maior vai inserir na ultima posicao
+            # por que aconteceu dele inserir na nova ultima mais um
+            if i == self.ultima_posicao:
+                posicao = i + 1
+
+        # logo em seguida preciso realizar o seguinte agora eu vou ir da ultima_posicao ate a posicao que eu vou inserir para remanejar
+        # eu aumentei o tamanho do array e passei o ultimo valor para ele na primeira interação
+        # todos ficaram um para frente ate chegar naquele  valor
+        #
+        # logo em seguida ate chegar na posicao que eu tenho que inserir que agora ficou duplicada tanto na posicao dela quanto na posicao a frente
+        # por fim eu preciso agora somente inserir na posicao onde o valor vai ficar para remover a duplicidade e ajustar
+        x = self.ultima_posicao
+        while x >= posicao:
+            self.valores[x + 1] = self.valores[x]
+            x -= 1
+
+        self.valores[posicao] = valor
+
+    def pesquisar(self, valor):
+        for i in range(self.ultima_posicao + 1):
+            if self.valores[i] > valor:
+                return -1
+            if self.valores[i] == valor:
+                return i
+            if i == self.ultima_posicao:
+                return -1
+        return -1
+
+    def excluir(self, valor):
+        posicao = self.pesquisar(valor)
+        if posicao != -1:
+            x = self.ultima_posicao
+            while x >= posicao:
+                self.valores[x + 1] = self.valores[x]
+                x -= 1
+
+            self.ultima_posicao -= 1
+        return -1
+
+
+vetor = VetorOrdenado(10)
+vetor.inserir(1)
+vetor.inserir(2)
+vetor.inserir(3)
+vetor.excluir(3)
+vetor.imprime()
---
+++ Vector/ordened/init.py
@@ -0,0 +1,27 @@
+# Os dados estão organizados na ordem ascendente do menor valor para o valor maior
+# Ou seja com o menor valor do indice 0 e cada celula maior que a celula abaixo
+# Operações de inserção em um vetor odernado precisamos de manter a ordem desses dados
+# O (n)
+#
+# Precisamos realizar uma pesquisa
+# n/2 pior caso #
+#
+# Realizar o remanejamento dos itens
+# n/2
+# n
+#
+# valor que eu inserir ele e maior ? passa
+# ele e menor acredita-se que o certo seria inserir mas, eu necessito organizar os demais
+# ele assume a posição do numero maior que ele possui e coloca os outros para direita
+#
+###  Pesquisa linear
+# Pesquisa termina quando o primeiro item maior que o valor de pesquisa e antigido
+# Como o vetor está ordenado, o algoritmo sabe que não há necessidade de procurar mais
+# Pior caso: Se o elemento não estiver no vetor ou na ultima posição
+#
+# Exclusao ele muda o tamanho da posição do array mas continua so remanejamento de
+# olha onde estava o indice que quer se excluido vai receber o indice do seu proximo e etc
+# So que ele pode para na metade se não encontrar o item no caminho.
+# Pior caso O - O(2n)
+# O (n)
+#
