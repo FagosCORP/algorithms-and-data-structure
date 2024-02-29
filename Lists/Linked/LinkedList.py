d815a590c8ebe7f1c92cb55dcbd907a54f7c53a7
ryan <hurleypvp1@gmail.com>
2024-02-29 20:20:22 -0300

    Finalizando aulas lista encadeada

---
+++ Lists/Linked/LinkedList.py
@@ -0,0 +1,27 @@
+from Lists.Linked.Node import Node
+import numpy as np
+
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.first = None
+
+    def add(self, value):
+        novo = Node(value)
+        # setando em o novo no (o no antigo)
+        novo.setNext(self.first)
+        # o novo primeiro vai virar o novo que contem o no antigo
+        self.first = novo
+
+    def print_list(self):
+        current = self.first
+        while current != None:
+            print(current.print_node())
+            current = current.getNext()
+
+    def remove(self):
+        if not self.first:
+            return None
+        temp = self.first
+        self.first = self.first.getNext()
+        return temp
---
+++ Lists/Linked/Node.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class Node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next = None
+
+    def setNext(self, next):
+        self.next = next
+
+    def getNext(self) -> None:
+        return self.next
+
+    def print_node(self):
+        print(self.value)
