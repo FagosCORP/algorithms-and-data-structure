219082137753be39ba429adf9b5d11927b44ac7d
ryan <hurleypvp1@gmail.com>
2024-03-07 20:40:00 -0300

    Finalizando aulas lista encadeada- interligada

---
+++ Lists/Double-Linked/init.py
@@ -0,0 +1,13 @@
+# Lista com link em extremidades duplas.
+# Da mesma forma que o inicio da Lista. (Cabeça)
+#
+# Agora possuiremos o ao final da lista. (Cauda)
+#
+# Para inserir um nó ao final da lista com extermidades.
+# E necessario percorrer todos elementos.
+# A referencia para o ultimo nó final da lista, assim como ao inicio.
+# Operações:
+# Inserir no inicio com o ponteiro para o proximo.
+# Inserir ao final com o ponteiro para o final.
+# Excluir do inicio
+# Não da para excluir o final pois ele não possui a referencia do anterior
